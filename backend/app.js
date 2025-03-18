ec79ab289de5144bf82cebfa20fa5e6ab63ec225
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-03-18 10:58:53 +0700

    Completed User Routes

--- backend/app.js
+++ backend/app.js
@@ -4,28 +4,33 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { connection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./router/userRoutes.js"
 
 const app = express();
 config({
-    path: "./config/config.env"
+  path: "./config/config.env"
 });
 
 app.use(cors({
-        origin: [process.env.FRONTEND_URL],
-        methods: ["POST", "GET", "PUT", "DELETE"],
-        credentials: true,
-    })
+  origin: [process.env.FRONTEND_URL],
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true,
+})
 );
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    fileUpload({
-      useTempFiles: true,
-      tempFileDir: "/tmp/",
-    })
-  );
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
 
 connection();
+app.use(errorMiddleware);
+
 export default app;
