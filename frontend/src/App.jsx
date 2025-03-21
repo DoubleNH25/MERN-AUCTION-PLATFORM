39bad5162c5e35576e8a3f45c420a0b10e3b11c7
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-03-21 11:16:27 +0700

    Completed Sidedrawer, Login, Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SideDrawer from './layout/SideDrawer';
 import Home from './pages/Home';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
-const App = ()=> {
+const App = () => {
   return (
     <Router>
-      <SideDrawer/>
+      <SideDrawer />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <ToastContainer position="top-right" />
     </Router>
