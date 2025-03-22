0ace2d10f3149f802e9767aa98b390a92259b61c
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-03-22 07:02:32 +0700

    completed homepage with implement auctionitem and leaderboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import SideDrawer from './layout/SideDrawer';
-import Home from './pages/Home';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SideDrawer from "./layout/SideDrawer";
+import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import SubmitCommission from "./pages/SubmitCommission";
+import { useDispatch } from "react-redux";
+import { fetchLeaderboard, fetchUser } from "./store/slices/userSlice";
+import HowItWorks from "./pages/HowItWorks";
+import About from "./pages/About";
+import { getAllAuctionItems } from "./store/slices/auctionSlice";
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUser());
+    dispatch(getAllAuctionItems());
+  }, []);
   return (
     <Router>
       <SideDrawer />
@@ -18,6 +29,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/submit-commission" element={<SubmitCommission />} />
         <Route path="/how-it-works-info" element={<HowItWorks />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <ToastContainer position="top-right" />
     </Router>
